c50d141e8eccbe2ec8d961834c0e46f2a73a71df
yuran1811 <trieuvanbd123@gmail.com>
2026-06-08 17:06:15 +0700

    chore: prepare template

--- run_servers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-killall node
-cd ./backend && pnpm i && pnpm dev &
-cd ./frontend-web && pnpm i && pnpm dev &
-cd ./frontend-admin && pnpm i && pnpm dev
---
+++ scripts/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+killall node
+cd ./backend && pnpm dev &
+cd ./frontend-web && pnpm dev &
+cd ./frontend-admin && pnpm dev
