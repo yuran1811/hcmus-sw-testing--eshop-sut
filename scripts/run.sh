7bde92879d1fe05d78b3c64c861d8e3bee47f527
NguyenAn0808 <23127148@student.hcmus.edu.vn>
2026-08-02 09:30:01 +0700

    chore: change local web host to 0.0.0.0 in run.sh to allow external access

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 killall node
 cd ./backend && pnpm dev &
-cd ./frontend-web && pnpm dev &
-cd ./frontend-admin && pnpm dev
+cd ./frontend-web && pnpm dev --host 0.0.0.0 &
+cd ./frontend-admin && pnpm dev --host 0.0.0.0
